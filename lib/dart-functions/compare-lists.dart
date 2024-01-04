34de6a1ce888377bd22bc2aa4302b6fb03fdd2f7
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2024-01-04 05:05:43 +0500

    Day 4 : Added Split First and Last Name Function

--- lib/dart-functions/compare-lists.dart
+++ lib/dart-functions/compare-lists.dart
@@ -7,3 +7,17 @@ Set<int> compare_lists()
   var list2 = [1,2,3,4,5];
  return list1.toSet();
 }
+
+String? splitName(
+    String name,
+    bool isFirstName,
+    ) {
+  final splittedName = name.split(' ');
+  if (splittedName.length < 2) {
+    return name;
+  }
+  if (isFirstName) {
+    return splittedName[0]; //retuns First Name
+  }
+  return splittedName[1]; // returns Last Name
+}
