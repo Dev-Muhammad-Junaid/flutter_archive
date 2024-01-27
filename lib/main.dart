2a5a1a712b8afdf3faf3fc16f4c8a6a2a9e405c2
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2024-01-27 21:40:16 +0500

    Day 24 : Updated Animated Button Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,61 +12,88 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.black,
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              AnimatedButton(
-                  text: "",
-                  gradientStyle: "Trident",
-                  containerWidth: 200,
-                  // Custom width
-                  containerHeight: 100,
-                  // Custom height
-                  containerMargin: const EdgeInsets.all(1),
-                  // Custom margin
-                  containerPadding: const EdgeInsets.all(20),
-                  // Custom padding
-                  containerColor: Colors.black38,
-                  // Custom background color
-                  containerOpacity: 0.8,
-                  // Custom opacity
-                  borderRadius: 20,
-                  // Custom border radius
-                  animationDuration: Duration(seconds: 2),
-                  textStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )),
-              AnimatedButton(
-                  text: "",
-                  gradientStyle: "PastelDreams",
-                  containerWidth: 200,
-                  // Custom width
-                  containerHeight: 400,
-                  // Custom height
-                  containerMargin: const EdgeInsets.all(2),
-                  // Custom margin
-                  containerPadding: const EdgeInsets.all(20),
-                  // Custom padding
-                  containerColor: Colors.white10,
-                  // Custom background color
-                  containerOpacity: 0.8,
-                  // Custom opacity
-                  borderRadius: 20,
-                  // Custom border radius
-                  animationDuration: Duration(seconds: 2),
-                  textStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )),
-
-            ],
+          padding: const EdgeInsets.all(50.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(padding: const EdgeInsets.all(10.0),),
+                AnimatedButton(
+                    text: "Hello",
+                    gradientStyle: "Trident",
+                    containerWidth: 200,
+                    // Custom width
+                    containerHeight: 100,
+                    // Custom height
+                    containerMargin: const EdgeInsets.all(1),
+                    // Custom margin
+                    containerPadding: const EdgeInsets.all(20),
+                    // Custom padding
+                    containerColor: Colors.black38,
+                    // Custom background color
+                    containerOpacity: 0.8,
+                    // Custom opacity
+                    borderRadius: 20,
+                    // Custom border radius
+                    animationDuration: Duration(seconds: 2),
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Padding(padding: const EdgeInsets.all(10.0),),
+                AnimatedButton(
+                    text: "World",
+                    gradientStyle: "PastelDreams",
+                    containerWidth: 200,
+                    // Custom width
+                    containerHeight: 200,
+                    // Custom height
+                    containerMargin: const EdgeInsets.all(2),
+                    // Custom margin
+                    containerPadding: const EdgeInsets.all(20),
+                    // Custom padding
+                    containerColor: Colors.white10,
+                    // Custom background color
+                    containerOpacity: 0.8,
+                    // Custom opacity
+                    borderRadius: 20,
+                    // Custom border radius
+                    animationDuration: Duration(seconds: 2),
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Padding(padding: const EdgeInsets.all(10.0),),
+                AnimatedButton(
+                    text: "Junaid",
+                    gradientStyle: "MysticalTwilight",
+                    containerWidth: 200,
+                    // Custom width
+                    containerHeight: 200,
+                    // Custom height
+                    containerMargin: const EdgeInsets.all(2),
+                    // Custom margin
+                    containerPadding: const EdgeInsets.all(20),
+                    // Custom padding
+                    containerColor: Colors.black,
+                    // Custom background color
+                    containerOpacity: 0.1,
+                    // Custom opacity
+                    borderRadius: 0,
+                    // Custom border radius
+                    animationDuration: Duration(seconds: 2),
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ],
+            ),
           ),
         ),
       ),
